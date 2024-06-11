d2566e2db714754663e14152901b480f9a964d27
vasmart24 <vasmart24@yandex.ru>
2024-06-11 19:05:06 +0500

    add Resources view in browser

--- Stranded/src/App.jsx
+++ Stranded/src/App.jsx
@@ -1,12 +1,12 @@
 // import Button from "./components/Button/Button";
 // import Resources from "./components/Header/Resources";
 import { useState } from 'react';
-import { baseResources } from './resources/baseResources';
+import { baseResources } from './resources/baseResources.js';
 
-function Resources() {
+function Resources(props) {
   return (
     <li>
-      <p>Древесина: 0</p>
+      <p>{props.name}: {props.count}</p>
     </li>
   )
 }
@@ -17,9 +17,9 @@ export default function App() {
       <main>
         <div>
           <ul>
-            <Resources></Resources>
-            <Resources></Resources>
-            <Resources></Resources>
+            <Resources name={baseResources[0].name} count={baseResources[0].count}></Resources>
+            <Resources name={baseResources[1].name} count={baseResources[1].count}></Resources>
+            <Resources name={baseResources[2].name} count={baseResources[2].count}></Resources>
           </ul>
         </div>
       </main>
--- Stranded/src/resources/baseResources.js
+++ Stranded/src/resources/baseResources.js
@@ -1,4 +1,6 @@
 export const baseResources = [
   { key: 0, name: "wood", count: 0 },
-  { key: 1, name: "iron", count: 0 },
+  { key: 1, name: "stone", count: 0 },
+  { key: 2, name: "iron", count: 0 },
 ];
+
