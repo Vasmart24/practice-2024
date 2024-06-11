4acf189c56c3950897af0b5cc9534866ee1df90d
toDoListEnjoyer <toDoListEnjoyer@gmail.com>
2024-06-11 16:54:47 +0300

    add Resources functionality

--- Stranded/src/App.jsx
+++ Stranded/src/App.jsx
@@ -1,55 +1,50 @@
 // import Button from "./components/Button/Button";
 // import Resources from "./components/Header/Resources";
-import { useState } from 'react';
-import { baseResources } from './resources/baseResources';
-
-function Resources() {
-  return (
-    <li>
-      <p>Древесина: 0</p>
-    </li>
-  )
-}
+import { useState } from "react";
+import { baseResources } from "./resources/baseResources";
 
 export default function App() {
+  const [resources, setResources] = useState(baseResources);
   return (
     <div>
       <main>
-        <div>
-          <ul>
-            <Resources></Resources>
-            <Resources></Resources>
-            <Resources></Resources>
-          </ul>
-        </div>
+        <Resources resources={resources} />
       </main>
     </div>
-  )
+  );
 }
 
+function Resources({ resources }) {
+  return (
+    <ul>
+      {resources.map((res) => (
+        <li key={res.key}>
+          {res.name}: {res.count}
+        </li>
+      ))}
+    </ul>
+  );
+}
 
+// const [isActionsModalOpened, setIsActionsModalOpened] = useState(false);
 
+// function handleActions() {
+//   setIsActionsModalOpened((prevState) => !prevState);
+// }
 
+// function handleIncreaseResource(e) {
+//   const increasedResId = Number(e.target.id);
 
- // const [isActionsModalOpened, setIsActionsModalOpened] = useState(false);
-
-  // function handleActions() {
-  //   setIsActionsModalOpened((prevState) => !prevState);
-  // }
-
-  // function handleIncreaseResource(e) {
-  //   const increasedResId = Number(e.target.id);
-
-  //   setResources(
-  //     [...resources].map((resource) => {
-  //       if (resource.id === increasedResId) {
-  //         const newCount = resource.count + 1;
-  //         return {
-  //           ...resource,
-  //           count: newCount,
-  //         };
-  //       }
-  //       return resource;
-  //     })
-  //   );
-  // }
+//   setResources(
+//     [...resources].map((resource) => {
+//       if (resource.id === increasedResId) {
+//         const newCount = resource.count + 1;
+//         return {
+//           ...resource,
+//           count: newCount,
+//         };
+//       }
+//       return resource;
+//     })
+//   );
+// }
