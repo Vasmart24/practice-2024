08b1b26f9b1deba29cd0008b85b5773953fa999c
Nikita Gulyaev <mikitka.glv@gmail.com>
2024-06-13 16:38:35 +0300

    add range slider

--- Stranded/src/screens/ScreenSearch.jsx
+++ Stranded/src/screens/ScreenSearch.jsx
@@ -1,9 +1,38 @@
 import Button from '../components/Button/Button.jsx';
+import React, { useState } from 'react';
 
-export default function ScreenSearch () {
-    return(
-        <div>
-            <Button>Начать поиск</Button>
-        </div>
-    )
-};
+// Объявляем компонент RangeSlider
+const RangeSlider = () => {
+    const [value, setValue] = useState(8); // Начальное значение ползунка
+  
+    const handleChange = (event) => {
+      setValue(event.target.value); // Обновляем значение стейта при изменении ползунка
+    };
+  
+    return (
+      <div>
+        <form>
+          <label htmlFor="rangeSlider">Время поиска ресурсов: {value}</label>
+          <input
+            id="rangeSlider"
+            type="range"
+            min="0"
+            max="16"
+            value={value}
+            onChange={handleChange}
+          />
+        </form>
+      </div>
+    );
+  };
+  
+  // Главный компонент ScreenSearch, в который включаем RangeSlider
+  export default function ScreenSearch () {
+    return (
+      <div>
+        <h1>React Ползунок</h1>
+        <RangeSlider />
+        <Button>Начать поиск</Button>
+      </div>
+    );
+  };
