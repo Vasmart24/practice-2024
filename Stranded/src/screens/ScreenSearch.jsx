f8e6eccefc1b499c9929804c107d29bb505c2f37
Nikita Gulyaev <mikitka.glv@gmail.com>
2024-06-13 14:47:46 +0300

    add screens

---
+++ Stranded/src/screens/ScreenSearch.jsx
@@ -0,0 +1,9 @@
+import Button from '../components/Button/Button.jsx';
+
+export default function ScreenSearch () {
+    return(
+        <div>
+            <Button>Начать поиск</Button>
+        </div>
+    )
+};
