c5121f263b70de11f136f4af670581471e66a0b8
driveGosling <toDoListEnjoyer@gmail.com>
2024-06-14 15:05:30 +0300

    add GameScreen component

--- Stranded/src/Game.jsx
+++ Stranded/src/Game.jsx
@@ -3,14 +3,21 @@ import ScreenSearch from "./screens/SearchResourcesScreen/ScreenSearch.jsx";
 import ScreenGetResources from "./screens/GetResourcesScreen/ScreenGetResources.jsx";
 import { useState } from "react";
 import { baseResources } from "./Data/headerData/materialsData.js";
+import GameScreen from "./screens/GameScreen/GameScreen.jsx";
 
-export default function Game() 
-{
+export default function Game() {
+  const [screen, setScreen] = useState();
+
+  function changeScreen(active) {
+    setScreen(active);
+  }
+
   return (
     <>
       <MainScreen></MainScreen>
       <ScreenSearch />
       <ScreenGetResources />
+      <GameScreen active={screen} />
     </>
   );
 }
--- Stranded/src/screens/MainScreen/MainScreen.jsx
+++ Stranded/src/screens/MainScreen/MainScreen.jsx
@@ -1,12 +1,12 @@
-import "./MainScreen.css"
-import MainScreenFooter from "../../components/MainScreenFooter/MainScreenFooter"
-import MainHeader from "../../components/MainHeader/MainHeader"
+import "./MainScreen.css";
+import MainScreenFooter from "../../components/MainScreenFooter/MainScreenFooter";
+import MainHeader from "../../components/MainHeader/MainHeader";
 
-export default function MainScreen({counts}) {
-  return(
+export default function MainScreen({ counts }) {
+  return (
     <div className="mainview">
       <MainHeader counts={counts}></MainHeader>
       <MainScreenFooter></MainScreenFooter>
     </div>
-  )
+  );
 }
