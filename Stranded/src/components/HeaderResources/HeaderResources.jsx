c845dfd83cbdc41c3a6ccf97ac50477407e5e7b7
Denis B <sinoptikgad@mail.ru>
2024-06-14 13:42:54 +0300

    create cssStyles branch

--- Stranded/src/components/HeaderResources/HeaderResources.jsx
+++ Stranded/src/components/HeaderResources/HeaderResources.jsx
@@ -1,9 +1,10 @@
 import { baseResources } from "../../resources/baseResources";
+import "./HeaderResources.css"
 
 export default function HeaderResources({ counts }) {
   console.log("мужа", counts);
   return (
-    <ul>
+    <ul className="list-item list-item_resource">
       {baseResources.map(({ key, name }) => {
         return (
           <p key={key}>
--- Stranded/src/screens/ScreenGetResources.jsx
+++ Stranded/src/screens/ScreenGetResources.jsx
@@ -2,15 +2,16 @@ import StartResButton from "../components/Button/resourcesScreenButton/StartResB
 import EasyResButton from "../components/Button/resourcesScreenButton/EasyResButton";
 import MediumResButton from "../components/Button/resourcesScreenButton/MediumResButton";
 import HardResButton from "../components/Button/resourcesScreenButton/HardResButton";
+import "./ScreenGetResources.css"
 
 export default function ScreenGetResources({ increaseCount }) {
   console.log("мы в ScreenGetResources!!!", increaseCount);
   return (
-    <div>
-      <StartResButton increaseCount={increaseCount}></StartResButton>
-      <EasyResButton increaseCount={increaseCount}></EasyResButton>
-      <MediumResButton increaseCount={increaseCount}></MediumResButton>
-      <HardResButton increaseCount={increaseCount}></HardResButton>
+    <div className="modal_level">
+      <div className="level-item offset_left"><StartResButton increaseCount={increaseCount}/></div>
+      <div className="level-item offset_right"><EasyResButton increaseCount={increaseCount}/></div>
+      <div className="level-item offset_left"><MediumResButton increaseCount={increaseCount}/></div>
+      <div className="level-item offset_right"><HardResButton increaseCount={increaseCount}/></div>
     </div>
   );
 }
