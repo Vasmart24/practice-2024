578c232781abc990050a0751d2913fa54cfd9964
vasmart24 <vasmart24@yandex.ru>
2024-06-15 21:02:45 +0300

    add screen change Buttons

--- Stranded/src/Game.jsx
+++ Stranded/src/Game.jsx
@@ -1,21 +1,19 @@
-// import { useState } from "react";
-// import GameScreen from "./screens/GameScreen/GameScreen.jsx";
-// import Header from "./components/Header/Header.jsx";
-import BattleScreen from "./screens/BattleScreen/BattleScreen.jsx";
-
-// export default function Game() {
-//   const [screen, setScreen] = useState("start");
-
-//   return (
-//     <>
-//       <Header />
-//       <GameScreen active={screen} setScreen={setScreen} />
-//     </>
-//   );
-// }
+import { useState } from "react";
+import GameScreen from "./screens/GameScreen/GameScreen.jsx";
+import Header from "./components/Header/Header.jsx";
+import Button from "./components/Button/Button.jsx";
 
 export default function Game() {
-  return(
-    <BattleScreen/>
-  )
+  const [screen, setScreen] = useState("start");
+
+  return (
+    <>
+      <Header />
+      <GameScreen active={screen} setScreen={setScreen} />
+      <Button onClick={() => setScreen('start')}>start screen</Button>
+      <Button onClick={() => setScreen('resources')}>resources screen</Button>
+      <Button onClick={() => setScreen('search')}>search screen</Button>
+      <Button onClick={() => setScreen('equipment')}>equipment screen</Button>
+    </>
+  );
 }
--- Stranded/src/screens/GameScreen/GameScreen.jsx
+++ Stranded/src/screens/GameScreen/GameScreen.jsx
@@ -1,6 +1,7 @@
-import GetResourcesScreen from "../GetResourcesScreen/GetResourcesScreen";
 import MainScreen from "../MainScreen/MainScreen";
+import GetResourcesScreen from "../GetResourcesScreen/GetResourcesScreen";
 import SearchScreen from "../SearchResourcesScreen/SearchScreen";
+import BattleScreen from "../BattleScreen/BattleScreen";
 import "./GameScreen.css";
 
 export default function GameScreen({ active, setScreen }) {
@@ -9,6 +10,7 @@ export default function GameScreen({ active, setScreen }) {
       {active === "resources" && <GetResourcesScreen />}
       {active === "start" && <MainScreen />}
       {active === "search" && <SearchScreen />}
+      {active === "equipment" && <BattleScreen />}
     </>
   );
 }
