5e66bdd5769394c8f4226e5bd6a7660e750e49c8
Nikita Gulyaev <mikitka.glv@gmail.com>
2024-06-19 23:56:09 +0300

    edit styles for ConstructionScreen

--- Stranded/src/components/Button/constructionScreenButtons/FarmsButton.jsx
+++ Stranded/src/components/Button/constructionScreenButtons/FarmsButton.jsx
@@ -2,12 +2,12 @@ import "./farmsButton.css";
 import Button from "../Button";
 import farmsData from "../../../Data/baseData/farmsData";
 
-function FarmButtonLvl1({ onClick, disabled}) {
-    const farmButtonClass = `farm-button`;
+function FarmButtonLvl1({ onClick, disabled }) {
+    const farmButtonClass = `buildings-button farm-button`;
     return (
         <Button onClick={onClick} className={farmButtonClass} disabled={disabled}>
-            <div>Ур: 1</div>
-            <div>Шт: {farmsData.lvl1.farmslvl1}</div>
+            <div className="buildings-button-header"><span>Ур: 1</span><span>&#10506;</span></div>
+            <div className="buildings-button-header"><span>Шт: {farmsData.lvl1.farmslvl1}</span><span>+</span></div>
         </Button>
     );
 }
