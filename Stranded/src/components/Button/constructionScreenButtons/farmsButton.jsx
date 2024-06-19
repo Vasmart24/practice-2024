a21c57cdceab762c6a6ca7c87b6246592b88316a
Nikita Gulyaev <mikitka.glv@gmail.com>
2024-06-19 18:26:00 +0300

    add CunstructionScreen

---
+++ Stranded/src/components/Button/constructionScreenButtons/farmsButton.jsx
@@ -0,0 +1,12 @@
+import "farmsButton.css";
+import Button from "../Button";
+
+export default function farmButton({ onClick, disabled }) {
+
+    return (
+        <Button onClick={onClick} className={} disabled={disabled}>
+            <div>Ур0</div>
+            <div>Шт0</div>
+        </Button>
+    );
+}
---
+++ Stranded/src/screens/ConstructionScreen/ConstructionScreen.jsx
@@ -0,0 +1,10 @@
+import "ConstructionScreen.css";
+import "../../components/Button/constructionScreenButtons/farmsButton";
+
+export default function ConstructionScreen({}) {
+    return (
+        <div className="construction-screen">
+            <farmButton></farmButton>
+        </div>
+    )
+}
